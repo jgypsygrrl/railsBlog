edae87ded17871e07c2b72920b9775fd51130002
Jennifer Gapay <jennifergapay@gmail.com>
2015-10-17 00:10:03 -0400

    railsBlog: List articles feature - test & cleared

---
+++ spec/features/listing_articles_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Articles" do
+
+  before do
+    @article1 = Article.create(title: "First Article", body: "This is the first article")
+    @article2 = Article.create(title: "Second Article", body: "This is a test of article 2")
+  end
+
+  scenario "List all articles" do
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+end
